59a6cbcbaf1ae66865a8254912b7e4abf0149ead
giulianogiova20 <94998931+giulianogiova20@users.noreply.github.com>
2022-09-13 23:48:22 -0300

    fixed mongoDB URI

--- api/config/mongoDBatlas.ts
+++ api/config/mongoDBatlas.ts
@@ -7,6 +7,6 @@ const CLUSTER = process.env.MONGOCLUSTER
 
 export default {
     mongoDB: {
-      URI: `mongodb+srv://${USR}:${PWD}@${CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
+      URI: `mongodb://${USR}:${PWD}@cluster0-shard-00-00.d9grp.mongodb.net:27017,cluster0-shard-00-01.d9grp.mongodb.net:27017,cluster0-shard-00-02.d9grp.mongodb.net:27017/?ssl=true&replicaSet=atlas-ondxdw-shard-0&authSource=admin&retryWrites=true&w=majority`,
     }
   }
